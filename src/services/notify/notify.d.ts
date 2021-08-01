58aa2b2cc1471311e3e5c0de0c70a6c0eaa312e0
aslanator <aslanator@mail.ru>
2021-08-01 12:58:51 +0300

    add axios

---
+++ src/services/notify/notify.d.ts
@@ -0,0 +1,7 @@
+export const notify = {
+  show (name: string, message: stirng): void,
+  success (name: string, message: stirng): void,
+  warning (name: string, message: stirng): void,
+  error (name: string, message: stirng): void
+}
+
